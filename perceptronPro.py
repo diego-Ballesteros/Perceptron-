a3fa6f922ae3b27e6345050f27782625b85cfb45
diegoballesteros <diegoballesteros777@gmail.com>
2023-10-24 13:00:40 -0500

    Modificando producto punto

--- perceptronPro.py
+++ perceptronPro.py
@@ -6,10 +6,10 @@ def main():
     #parametros iniciales sugeridos
     initial_weights = [0, -0.5, 2] 
     rate_n = 0.2
-    iter_max = 200
+    iter_max = 2000
    
     points = [
-                [0, 14, 2], [0,16, 5], [0,17, 10], [0,19, 3],
+                [0, 14, 2], [0,16, 5], [0,17, 10], [0,19, 3],[0,14,18],
                 [1,6, 9], [1, 8, 14], [1, 9, 7], [1, 9, 11], [1, 11, 8], 
              ]
     
@@ -31,7 +31,7 @@ def main():
 # 0 -> p(sumatoria)<=0
 def perceptron_output(weights, point):
     #producto punto 
-    summation = (weights[0]*point[0] + weights[1]*point[1] + weights[2]*point[2])
+    summation = (weights[0]*1 + weights[1]*point[1] + weights[2]*point[2])
     if summation > 0:
         return 1
     else:
@@ -73,6 +73,8 @@ def perceptron (weights, points, rate_n, max_iter):
         for point in points:
             current_weights, change_weight = new_weights(current_weights, point, rate_n, change_weight)
         current_iter += 1
+        if current_iter == max_iter:
+            print(" !! No se encontro solucion para clasificar este caso")
     return current_weights, current_iter
 
 
